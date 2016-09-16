cdd5b8250b90d42a91c2ab4e8b2d42922611395b
katsuya.obara <katsuya0719@gmail.com>
2016-09-16 11:21:42 +0900

    hbar.jsは配列にそったscriptingが必要

--- static/js/hbar.js
+++ static/js/hbar.js
@@ -18,17 +18,80 @@ var xscale = d3.scale.linear()
 				.range([0,722]);
 
 var yscale = d3.scale.linear()
-				.domain([0,categories.length])
+				//.domain([0,room.length])
 				.range([0,480]);
 
 var colorScale = d3.scale.quantize()
-				.domain([0,categories.length])
+				//.domain([0,room.length])
 				.range(colors);
 
 var canvas = d3.select('#hbar')
 				.append('svg')
 				.attr({'width':900,'height':550});
 
+function hbarChart(csv){
+	d3.csv(csv,function(d,i){
+		var arr = Object.keys(d).map(function (key) {return d[key]});
+	    d.room = arr[0];
+	    d.area = +d["Area [m2]"];
+	    d.Light = +d["Lighting [W/m2]"];
+	    d.People = +d["People [m2 per person]"];
+	    d.Plug = +d["Plug and Process [W/m2]"];
+	    return d;
+	  }, function(error, data) {
+	  	//console.log(data)
+	    if (error) throw error;
+	    var len=Object.keys(data).length;
+	    var data1=[];
+	    data.forEach(function(d,i){
+	      if(i<len-5){
+	        data1.push(d);
+	      }
+	    });	   
+	console.log(data1);
+	yscale.domain(data1.map(function(d) { return d.room; }));
+	colorScale.domain(data1.map(function(d) { return d.room; }));
+    //yscale.domain([0, d3.max(data1, function(d) { return d.area; })]);
+
+	var	xAxis = d3.svg.axis();
+			xAxis
+			.orient('bottom')
+			.scale(xscale)
+			.tickValues(tickVals);
+
+	var	yAxis = d3.svg.axis();
+			yAxis
+			.orient('left')
+			.scale(yscale)
+			.tickSize(2)
+			.tickFormat(function(d,i){ return categories[i]; })
+			.tickValues(d3.range(17));
+
+	var y_xis = canvas.append('g')
+						  .attr("transform", "translate(150,0)")
+						  .attr('id','yaxis')
+						  //.call(yAxis);	
+						  .call(yscale);	
+
+	var x_xis = canvas.append('g')
+						  .attr("transform", "translate(150,480)")
+						  .attr('id','xaxis')
+						  .call(xAxis);
+
+	var chart = canvas.append('g')
+							.attr("transform", "translate(150,0)")
+							.attr('id','bars')
+							.selectAll('rect')
+							.data(data1)
+							.enter()
+							.append('rect')
+							.attr('height',19)
+							.attr({'x':0,'y':function(d,i){ return yscale(i)+19; }})
+							.style('fill',function(d,i){ return colorScale(i); })
+							.attr('width',function(d){ return 0; });
+	});
+};
+/*
 var grids = canvas.append('g')
 				  .attr('id','grid')
 				  .attr('transform','translate(150,10)')
@@ -43,43 +106,6 @@ var grids = canvas.append('g')
 					})
 				  .style({'stroke':'#adadad','stroke-width':'1px'});
 
-var	xAxis = d3.svg.axis();
-			xAxis
-			.orient('bottom')
-			.scale(xscale)
-			.tickValues(tickVals);
-
-var	yAxis = d3.svg.axis();
-			yAxis
-			.orient('left')
-			.scale(yscale)
-			.tickSize(2)
-			.tickFormat(function(d,i){ return categories[i]; })
-			.tickValues(d3.range(17));
-
-var y_xis = canvas.append('g')
-						  .attr("transform", "translate(150,0)")
-						  .attr('id','yaxis')
-						  .call(yAxis);
-
-		var x_xis = canvas.append('g')
-						  .attr("transform", "translate(150,480)")
-						  .attr('id','xaxis')
-						  .call(xAxis);
-
-		var chart = canvas.append('g')
-							.attr("transform", "translate(150,0)")
-							.attr('id','bars')
-							.selectAll('rect')
-							.data(dollars)
-							.enter()
-							.append('rect')
-							.attr('height',19)
-							.attr({'x':0,'y':function(d,i){ return yscale(i)+19; }})
-							.style('fill',function(d,i){ return colorScale(i); })
-							.attr('width',function(d){ return 0; });
-
-
 		var transit = d3.select("svg").selectAll("rect")
 						    .data(dollars)
 						    .transition()
@@ -93,3 +119,6 @@ var y_xis = canvas.append('g')
 							.append('text')
 							.attr({'x':function(d) {return xscale(d)-200; },'y':function(d,i){ return yscale(i)+35; }})
 							.text(function(d){ return d+"$"; }).style({'fill':'#fff','font-size':'14px'});
+*/
+
+hbarChart("static/csv/Nantou/Zone.csv")
